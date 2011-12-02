ad32495f575098ec91408be83ed3476590d8886f
Alex Nichol <alexnichol@comcast.net>
2011-12-02 15:59:56 -0500

    Initial Commit

---
+++ ObjectCoderTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ObjectCoderTest
+//
+//  Created by Alex Nichol on 12/2/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
